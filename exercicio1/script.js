feafaccaf7e4976357e77ddf8a2f02cead467a42
vitorramos <ramos6382@gmail.com>
2022-08-29 17:50:10 -0300

    exercicios a008

---
+++ exercicio1/script.js
@@ -0,0 +1,4 @@
+let pNumero = Number(prompt("Insira um numero par"))
+console.log (pNumero/2) 
+//Devolve um numero dividido por 2
+//devolve um numero quebrado
---
+++ exercicio3/script.js
@@ -0,0 +1,9 @@
+const ope1 = 5>20&&5<2
+const ope2 = 5===5||5==="5"
+const ope3 = 20>50
+const ope4 = 20>50||50>60
+
+console.log (ope1)
+console.log (ope2)
+console.log (!ope3)
+console.log (!ope4)
