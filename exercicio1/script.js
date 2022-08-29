0778f863aa08ea9384820ed787bce0a3b33f0856
vitorramos <ramos6382@gmail.com>
2022-08-29 17:56:46 -0300

    alteração do primeiro exercicio

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,4 +1,4 @@
 let pNumero = Number(prompt("Insira um numero par"))
-console.log (pNumero/2) 
-//Devolve um numero dividido por 2
-//devolve um numero quebrado
+console.log (pNumero%2) 
+//Devolve 0 para os numero pares e 1 para os impares
+//devolve 1
